a21e0a98128a0eba004f599497699abb5cee240c
evagolabek <golabek.eva@gmail.com>
2018-02-18 11:19:01 +0100

    added map contacts

--- main.rb
+++ main.rb
@@ -4,3 +4,9 @@ def read_contacts
   contacts_json = File.read("contacts.json")
   return JSON.parse(contacts_json, { symbolize_names: true })
 end
+
+def map_contacts
+  read_contacts.map { |contact| { contact[:name] => contact[:phone] } }
+end
+
+puts map_contacts
